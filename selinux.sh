56a6f24aaf683178f9bb8eb426f5c40256766961
Kyle Walker <kylewalker@Kyles-MacBook-Pro.local>
2020-04-27 13:39:09 -0500

    restorecon's added where needed

--- selinux.sh
+++ selinux.sh
@@ -19,5 +19,9 @@ git clone https://github.com/NVIDIA/dgx-selinux $tmp_dir/dgx-selinux
 # Inject newly compiled SELinux module
 semodule -i $tmp_dir/dgx-selinux/bin/RHEL7/nvidia-container.pp
 
+# Set contexts for SELinux
+nvidia-container-cli -k list | restorecon -v -f -
+restorecon -Rv /dev
+
 # Run test container with podman
 podman run --user 1000:1000 --network=host --security-opt label=type:nvidia_container_t mirrorgooglecontainers/cuda-vector-add:v0.1
