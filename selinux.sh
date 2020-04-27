bf0ca6e52a09e266d75a1a09f08a50e10b827d81
Kyle Walker <kylewalker@Kyles-MacBook-Pro.local>
2020-04-27 13:30:01 -0500

    file path update, comment update

--- selinux.sh
+++ selinux.sh
@@ -2,8 +2,7 @@
 
 ###
 #
-# A lot of work was done by Nvidia and Red Hat to work on these SELinux modules.
-# Their work can be found here
+# Based on the work found here:
 # https://github.com/NVIDIA/dgx-selinux
 #
 ###
@@ -18,7 +17,7 @@ yum install -y git
 git clone https://github.com/NVIDIA/dgx-selinux $tmp_dir/dgx-selinux
 
 # Inject newly compiled SELinux module
-semodule -i $tmp_dir/dgx-selinux/bin/RHEL7/nvidia-container-pp
+semodule -i $tmp_dir/dgx-selinux/bin/RHEL7/nvidia-container.pp
 
 # Run test container with podman
 podman run --user 1000:1000 --network=host --security-opt label=type:nvidia_container_t mirrorgooglecontainers/cuda-vector-add:v0.1
